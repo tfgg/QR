4d2bece23d12cf0a1588b4ce14a69e1b6346dff3
Tim Green <timothy.green@gmail.com>
2011-06-19 10:05:47 +0100

    put keys in local settings

--- core/models.py
+++ core/models.py
@@ -3,17 +3,12 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 import settings
+from settings import bitly_username, bitly_key
 
 import urllib
 import urllib2
 import json
 from datetime import datetime
-
-endpoint = "http://content.guardianapis.com"
-key = "uct3rztj38km5gcnwaeqmk3z"
-
-bitly_username = "caramelcarrot"
-bitly_key = "R_4b93f4028596afc8a62dd140188d3644"
 
 def bitly_shorten(url):
   url = urllib.quote(url)
